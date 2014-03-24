c43452ec51bcda3931086ac6f53ba2d1e6ee0db8
Grant Speelman <grant.speelman@ubxd.com>
2014-03-24 08:04:25 +0200

    0.2.3pre

--- lib/mxit_api/version.rb
+++ lib/mxit_api/version.rb
@@ -2,9 +2,8 @@ class MxitApi
   module Version
     MAJOR = 0
     MINOR = 2
-    PATCH = 2
+    PATCH = 3
     BUILD = 'pre'
-
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
 end
